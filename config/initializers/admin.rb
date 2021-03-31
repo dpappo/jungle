a59745045b04348f36bf6285f74e4c15156b736d
Dan Pappo <dpappo@gmail.com>
2021-03-31 13:27:54 -0400

    http basic auth

---
+++ config/initializers/admin.rb
@@ -0,0 +1,7 @@
+# Rails.configuration.admin = {
+#   :admin_username => ENV['ADMIN_USERNAME'],
+#   :admin_password      => ENV['ADMIN_PASSWORD']
+# }
+
+# Admin.user_name = Rails.configuration.admin[:admin_username]
+# Admin.password = Rails.configuration.admin[:admin_password]
